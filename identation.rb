2fc867156ec5f5456bde23b99eb09830c8d70267
YourSouLi5Mine <precichysim@gmail.com>
2020-01-29 14:41:42 -0600

    trainee vim exercises

---
+++ identation.rb
@@ -0,0 +1,14 @@
+class Calculator
+def sum(first_number, second_number)
+first_number + second_number
+end
+def substraction(first_number, second_number)
+first_number - second_number
+end
+def multiply(first_number, second_number)
+first_number * second_number
+end
+def divide(first_number, second_number)
+first_number / second_number
+end
+end
